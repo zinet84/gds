a270b6e67a5050734c4aa20639bcd4833ccc6da8
Your Name <ads>
2021-04-12 04:33:00 +0900

    a

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import os
 import pytesseract
 import discord
+from discord import message
 from discord.ext import commands
 
 TOKEN=os.environ["BOT_TOKEN"]
@@ -14,8 +15,11 @@ async def on_ready():
     await app.change_presence(status=discord.Status.online, activity=None)
     print("ready")
 
+aaa = cv2.imread("https://cdn.discordapp.com/attachments/500371271314571266/830877235895861248/3.png")
+print(f"image: {aaa}")
+
 @app.command()
 async def 로(ctx, *, text):
-    await ctx.send(text)
+    await ctx.send(f"{ctx.message}\n\n{ctx.kwargs}")
 
 app.run(TOKEN)
