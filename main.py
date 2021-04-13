adb9eed4062ee01daebe96244cf616c33ace1430
Your Name <ads>
2021-04-13 23:16:49 +0900

    a

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import cv2
 import os
-import pytesseract
+import pytesseract as pt
 import discord
-from discord import message
+from discord import message, Attachment
 from discord.ext import commands
+import numpy as np
+import requests
 
 TOKEN=os.environ["BOT_TOKEN"]
 
-app = commands.Bot(command_prefix='!수')
+app = commands.Bot(command_prefix='!')
 
 @app.event
 async def on_ready():
@@ -15,11 +17,28 @@ async def on_ready():
     await app.change_presence(status=discord.Status.online, activity=None)
     print("ready")
 
-aaa = cv2.imread("https://cdn.discordapp.com/attachments/500371271314571266/830877235895861248/3.png")
-print(f"image: {aaa}")
-
 @app.command()
-async def 로(ctx, *, text):
-    await ctx.send(f"{ctx.message}\n\n{ctx.kwargs}")
+async def 수로(ctx):
+    url = ctx.message.attachments[0].url
+    image_nparray = np.asarray(bytearray(requests.get(url).content), dtype=np.uint8)
+    image = cv2.imdecode(image_nparray, cv2.IMREAD_COLOR)
+    height, width, channels = image.shape
+    scale=4
+    image = cv2.resize(image,(int(width*scale),int(height*scale)))
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    image = cv2.dilate(image, kernel, anchor=(-1, -1), iterations=1)
+
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+    cv2.imwrite("/mnt/c/Users/simdh/Documents/test.jpg", image)
+
+    result = pt.image_to_string(image,lang='kor+eng')
+
+    print("="*30)
+    print()
+    #print(pt.image_to_boxes(image,lang='kor+eng'))
+    print("="*30)
+    await ctx.send(result)
 
 app.run(TOKEN)
